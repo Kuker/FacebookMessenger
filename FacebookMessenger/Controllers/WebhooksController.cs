d45bc0b3bce083c52eac4177817213f479968f8d
Radomir Nowak <kuker16@gmail.com>
2017-04-22 14:04:04 +0200

    Added post method for receiving json data

--- FacebookMessenger/Controllers/WebhooksController.cs
+++ FacebookMessenger/Controllers/WebhooksController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using FacebookMessenger.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace FacebookMessenger.Controllers
 {
@@ -34,7 +37,53 @@ namespace FacebookMessenger.Controllers
             }
 
             logger.LogError("Tokens do not match. Got: {0}. Expected: {1}", hubVerifyToken, webhookOptions.VerifyToken);
+            Response.StatusCode = (int) HttpStatusCode.Forbidden;
             return "Token verification error";
+        }
+
+        [HttpPost]
+        public void Webhooks()
+        {
+            string json;
+            try
+            {
+                using (StreamReader sr = new StreamReader(this.Request.Body))
+                {
+                    json = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical("Could not parse request body.", e);
+                return;
+            }
+
+            logger.LogDebug("Successfully read json from request body: {0}", json);
+            dynamic data = JsonConvert.DeserializeObject(json);
+
+            if (data.@object == "page")
+            {
+                foreach (var entry in data.entry)
+                {
+                    var pageId = entry.id;
+                    var timeOfEvent = entry.time;
+
+                    foreach (var evt in entry.messaging)
+                    {
+                        if (evt.message)
+                        {
+                            //todo
+                            logger.LogDebug("Webook received message event.");
+                        }
+                        else
+                        {
+                            logger.LogDebug("Webhook received unknown event: ", (string)evt);
+                        }
+                    }
+                }
+            }
+
+            Response.StatusCode = (int) HttpStatusCode.OK;
         }
     }
 }
